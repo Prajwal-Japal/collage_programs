ae8ad3e50fe26fd9945ea20db30b93ffaa8471a4
Prajwal Japal <prajwaljapal@gmail.com>
2026-09-09 15:27:52 +0530

    1

--- java/HabdelError/main.java
+++ java/HabdelError/main.java
@@ -3,14 +3,13 @@ class Handleerror{
     public static void main(String[] args) {
         int a=0, b=0, c=0;
         Random r = new Random();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             try {
                 b = r.nextInt();
                 c = r.nextInt();
-                a = 12345 / (b+c);
+                a = 12345 / (b/c);
             } catch (ArithmeticException e) {
-                System.out.println("Error: Division by zero or invalid operation. b=");
-
+                System.out.println("Error: Division by zero.");
             }
             System.out.println("a: " + a);
         }
