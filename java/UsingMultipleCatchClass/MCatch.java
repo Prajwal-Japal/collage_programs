b468ef0dd002bee8a97b46a077823fabd3b8f817
Prajwal Japal <prajwaljapal@gmail.com>
2026-09-09 15:40:49 +0530

    Using Multiple Catch Clause

---
+++ java/UsingMultipleCatchClass/MCatch.java
@@ -0,0 +1,15 @@
+class MCatch{
+    public static void main (String[] args){
+        try{
+            int a = args.length;
+            int b = 42/a;
+            int c[] = {1};
+            c[42] = 99;
+        }catch(ArithmeticException e){
+            System.out.println("Divide by 0: " + e);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Array index out of bounds: " + e);
+        }
+        System .out.println("After try/catch blocks.");
+    }
+}
